600c91c9bbc44c3dc8fdb579b932dfd5f3bdfc31
Tiago Poli <tiagoluizpoli@debian.debian-pc>
2024-12-05 01:18:06 -0300

    refactor(packages): remove prisma remains from the app

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,10 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-empty': [2, 'never'],
-    'scope-enum': [2, 'always', ['user-management', 'configs', 'dev-tools', 'packages', 'multiple', 'root']],
+    'scope-enum': [
+      2,
+      'always',
+      ['user-management', 'throne-management', 'configs', 'dev-tools', 'packages', 'multiple', 'root'],
+    ],
   },
 }
--- packages/shared-database/prisma/migrations/20240805194201_add_refresh_token_identifier_to_session_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `refreshTokenIdentifier` to the `session` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "session" ADD COLUMN     "refreshTokenIdentifier" VARCHAR(40) NOT NULL;
